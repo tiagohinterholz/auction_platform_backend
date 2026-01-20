70c7cf6aee6af842665e8f27b6cf4e0311e18e69
Tiago Hinterholz <fh.tiago@gmail.com>
2026-01-20 20:23:47 -0300

    Update structure creating modules folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { HealthModule } from './health/health.module';
+import { HealthModule } from './modules/health/health.module';
 import { DatabaseModule } from './shared/database/database.module';
-import { AuctionModule } from './auction/auction.module';
+import { AuctionModule } from './modules/auction/auction.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
