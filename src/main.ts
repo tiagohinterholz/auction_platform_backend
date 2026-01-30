8bd499bc7802aca3261d278754d1af1347ddfafd
Tiago Hinterholz <fh.tiago@gmail.com>
2026-01-30 08:05:18 -0300

    chore: include enableCORS to frontend development can access

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,12 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT') || 3000;
   await app.listen(port);
